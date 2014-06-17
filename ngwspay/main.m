72368b48777770af4d9694c759728f6a364179ed
decam2009 <decam2009@gmail.com>
2014-06-17 14:11:35 +0600

    Initial Commit

---
+++ ngwspay/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ngwspay
+//
+//  Created by Boris Kaloshin on 6/17/14.
+//  Copyright (c) 2014 Boris Kaloshin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
---
+++ ngwspayTests/ngwspayTests.m
@@ -0,0 +1,34 @@
+//
+//  ngwspayTests.m
+//  ngwspayTests
+//
+//  Created by Boris Kaloshin on 6/17/14.
+//  Copyright (c) 2014 Boris Kaloshin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ngwspayTests : XCTestCase
+
+@end
+
+@implementation ngwspayTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
